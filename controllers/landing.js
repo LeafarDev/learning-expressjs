1490d2f4cb8bd0dfb1d3b118aa5970dd7f352713
ew <rafael.o@aluno.ifsp.edu.br>
2020-10-14 23:00:03 -0300

    post route email

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,3 +1,8 @@
 exports.get_landing = (req, res, next) => {
-    res.render('landing', {title: 'Express 2'});
+    res.render('landing', {title: 'Express 2'})
+}
+
+exports.submit_lead = (req, res, next) => {
+    console.log("lead email", req.body.lead_email)
+    res.redirect('/')
 }
